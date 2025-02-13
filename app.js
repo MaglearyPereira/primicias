c2e5c205540ba9f518d3302df840975032b92cca
maglearypereira <maglearypereirao@gmail.com>
2025-02-13 16:00:03 -0400

    [ADD]STYLES

--- app.js
+++ app.js
@@ -12,7 +12,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
     res.render('index', { title: 'Mi App', message: '¡Hola, mundo!' });
 });
-
+app.get('/creceRoutes', (req, res) => {
+    res.render('crece', { title: 'Mi App', message: '¡Hola, mundo!' });
+});
+app.get('/fundamentos', (req, res) => {
+    res.render('fundamentos', { title: 'Mi App', message: '¡Hola, mundo!' });
+});
+app.get('/afilados', (req, res) => {
+    res.render('afilados', { title: 'Mi App', message: '¡Hola, mundo!' });
+});
 // Iniciar el servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
